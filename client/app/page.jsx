5cf2fc1346cee37e9398d8db5332767c78cd35a7
keshavgarg5130 <keshav.garg.5130@gmail.com>
2026-01-29 23:26:48 +0530

    next-auth

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,10 +1,11 @@
 import Room from "@/components/room";
 import UploadForm from "../components/uploadForm";
+import AuthPage from "../components/AuthPage";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <UploadForm/>
+        <AuthPage/>
     </div>
   );
 }
---
+++ client/components/AuthPage.jsx
@@ -0,0 +1,32 @@
+"use client"
+import React from 'react'
+import { useSession, signIn, signOut } from "next-auth/react"
+import {Button} from "./ui/button";
+
+const AuthPage = () => {
+    const { data } = useSession()
+    console.log('session data : ', data);
+
+    const logIn = async() => {
+        console.log("Signing in Google");
+        await signIn("google");
+    }
+
+    const logOut = async() => {
+        console.log("Signing out of Google");
+        await signOut();
+    }
+
+    return (
+        <div className='m-10'>
+            <Button variant={"outline"} className='bg-blue-300' onClick={logIn}>
+                Sign IN
+            </Button>
+            <Button variant={"destructive"} onClick={logOut}>
+                Sign Out
+            </Button>
+
+        </div>
+    )
+}
+export default AuthPage
