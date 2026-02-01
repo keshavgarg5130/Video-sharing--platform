b6a7eb237a610dd07c84b8ef5cd414fa37186862
keshavgarg5130 <keshav.garg.5130@gmail.com>
2026-02-02 00:29:40 +0530

    aws-multipart-test

--- upload_service/routes/upload.route.js
+++ upload_service/routes/upload.route.js
@@ -1,9 +1,10 @@
 import express from "express"
 import uploadFileToS3 from "../controllers/upload.controller.js";
 import multer from "multer"
+import multipartUploadFileToS3 from "../controllers/multipartupload.controller.js";
 
 const upload = multer();
 const router = express.Router();
-router.post('/', upload.single('file'), uploadFileToS3);
+router.post('/', multipartUploadFileToS3);
 
 export default router;
